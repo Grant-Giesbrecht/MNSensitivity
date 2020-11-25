33f3d3d8e4a1e817834134a293b4042024190fb5
Grant Giesbrecht <giesbrechtgrant@gmail.com>
2020-11-25 14:43:52 -0700

    MNSens: added Z_out() and more error checking. Fixed /0 bug

--- MNSensitivity.py
+++ MNSensitivity.py
@@ -115,24 +115,43 @@ class Component:
     def Z(self, freq:float):
 
         if self.type == Passive.C:
-            return self.val.R + self.Z_L(freq) + self.Z_C(freq)
+
+            if self.Z_C(freq) == None:
+                print(f"WARNING: Capacitor {self.name} has 0 capacitance. Will behave like R or L.")
+                return self.Z_L(freq) + self.val.R
+            else:
+                return self.val.R + self.Z_L(freq) + self.Z_C(freq)
+
         elif self.type == Passive.L:
-            return self.pall(self.Z_L(freq) + self.val.R, self.Z_C(freq))
+
+            if self.Z_C(freq) == None:
+                return self.Z_L(freq) + self.val.R
+            else:
+                return self.pall(self.Z_L(freq) + self.val.R, self.Z_C(freq))
         elif self.type == Passive.R:
-            return self.Z_L(freq) + self.pall(self.val.R, self.Z_C(freq))
+
+            if self.Z_C(freq) == None:
+                return self.Z_L(freq) + self.val.R
+            else:
+                return self.Z_L(freq) + self.pall(self.val.R, self.Z_C(freq))
+
         else:
             print(f"ERROR: component {self.name} does not have a valid type {self.type}.")
             return None
 
     def Z_C(self, f:float):
         """ Calculates the impedance of a capacitor """
+
+        if self.val.C == 0 or f == 0:
+            return None
+
         return complex(0, 1)/(2*3.14159*f*self.val.C)
 
     def Z_L(self, f:float):
         """ Calculates the impedance of a capacitor """
         return complex(0, 1)*(2*3.14159*f*self.val.L)
 
-    def pall(self, Z1:complex, z2:complex):
+    def pall(self, Z1:complex, Z2:complex):
         """ Calculates the impedance of two parallel elements """
         return Z1*Z2/(Z1+Z2)
 
@@ -150,6 +169,9 @@ class Component:
 
 
 def print_circuit(circ:list):
+    """ Prints the components in a Circuit List (format is 1D array of tuples,
+    the first element contains a Component object, the 2nd a SER/PAL string)
+    """
 
     for t in circ:
 
@@ -161,6 +183,19 @@ def print_circuit(circ:list):
             print("ERROR")
 
 def load_circuit(filename:str):
+    """ Reads a MNSensitivity cicuit file (.mc) and returns a Circuit list
+    (format is 1D array of tuples, the first element contains a Component
+    object, the 2nd a SER/PAL string).
+
+    Format of the .mc file is:
+        * each line contains a Component object init string (See Component class
+          doc string to see format) after an orientation string (SER or PAL,
+          specifies if the component is series or parallel to ground).
+        * Comments can be specified by '#'
+        * Blank lines are skipped
+        * Components with earliest line number is assumed closest to source,
+          last line number closest to load, and progressively inbetween.
+    """
 
     circuit = []
 
@@ -174,12 +209,20 @@ def load_circuit(filename:str):
 
             #Read line...
             line = file.readline()
-            lnum += 0;
+            lnum += 1;
             if not line:
                 break;
 
             #Break into tokens...
             words = line.split()
+
+            if len(words) == 0:
+                continue
+
+            #Skip comments
+            if words[0] == "#" or words[0][0] == '#':
+                continue
+
             if len(words) < 5:
                 print(f"ERROR: Fewer than 5 words on line {lnum}.")
                 print(words)
@@ -203,3 +246,51 @@ def load_circuit(filename:str):
                 return []
 
     return circuit
+
+def pall(Z1:complex, Z2:complex):
+    """ Calculates the impedance of two parallel elements """
+    return Z1*Z2/(Z1+Z2)
+
+def Z_out(circuit:list, freq:float, Zsource:complex):
+    """ Takes a circuit list (see load_circuit for format info) and frequency
+    and calculates the equivilient output impedance of the network.
+    """
+
+    Zout = Zsource
+    last_comp_name = None
+
+    for t in circuit:
+
+        #Verifies that the circuit element is valid
+        try:
+            #Read component
+            comp = t[0]
+            comp.Z_L(1) #Will fail if not a component class
+
+            #Read orientation
+            orientation = t[1]
+            if orientation.upper() != "SER" and orientation.upper() != "PAL":
+                print(f"ERROR: Invalid orientation '{orientation}' in circuit. Element: '{comp.name}'")
+                return None
+        except Exception as e:
+            print(f"ERROR: Invalid circuit element. {str(e)}")
+            try:
+                print(f"\tError occured with component: {comp.name}")
+            except:
+                if last_comp_name == None:
+                    print("\tError occured with first component.")
+                else:
+                    print(f"\tError occured because missing component object. Last valid component object: {last_comp_name}")
+
+            return None
+
+        #Save component name in case error occurs later and need to identify where
+        last_comp_name = comp.name
+
+        #If next element is parallel
+        if orientation == "PAL":
+            Zout = pall(Zout, comp.Z(freq))
+        elif orientation == "SER":
+            Zout = Zout + comp.Z(freq)
+
+    return Zout
